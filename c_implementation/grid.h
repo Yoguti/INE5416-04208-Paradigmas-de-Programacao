97988babea867810d4320bb8d06dd0004f59d5b8
Yoguti <klaus2027@gmail.com>
2025-04-22 16:49:15 -0300

    changes to grid.h

--- c_implementation/grid.h
+++ c_implementation/grid.h
@@ -1,14 +1,14 @@
-#ifndef GRID_H
-#define GRID_H
+    #ifndef GRID_H
+    #define GRID_H
 
-#include "cell.h"
+    #include "cell.h"
 
-typedef struct {
-    int id;
-    int size;
-    int region_rows;
-    int region_cols;
-    Cell** grid;  // Array 2D de Cells
-} Grid;
+    typedef struct {
+        int id;
+        int size;
+        int region_rows;
+        int region_cols;
+        Cell** grid;  // Array 2D de Cells
+    } Grid;
 
-#endif // GRID_H
+    #endif // GRID_H
